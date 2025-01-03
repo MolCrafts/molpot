bfe96638f52cf23828a2eb0df0e0a58626979157
Roy Kid <lijichen365@gmail.com>
2025-01-03 15:14:44 +0000

    refactor datapipeline without debug

--- src/molpot/pipline/dataloader.py
+++ src/molpot/pipline/dataloader.py
@@ -1,17 +1,29 @@
 from typing import Sequence
-
+from torch.utils.data import Dataset
 import torch
-from torch.utils.data import DataLoader
-
+import torchdata.nodes as tn
+from torch.utils.data import RandomSampler, SequentialSampler, default_collate, Dataset
 from molpot import Config, Frame, alias
+from functools import reduce
+
+
+def compose(*funcs):
+    """Compose a list of functions into a single function."""
+    return lambda x: reduce(lambda v, f: f(v), funcs, x)
+
 
 def cancel_batch(tensor_or_nested_tensor: torch.Tensor):
     if isinstance(tensor_or_nested_tensor, torch.Tensor):
         if tensor_or_nested_tensor.is_nested:
             return torch.concat([td for td in tensor_or_nested_tensor])
         else:
-            return tensor_or_nested_tensor.reshape(-1, *tensor_or_nested_tensor.shape[2:])
-    return tensor_or_nested_tensor.apply(cancel_batch, batch_size=[], call_on_nested=True)
+            return tensor_or_nested_tensor.reshape(
+                -1, *tensor_or_nested_tensor.shape[2:]
+            )
+    return tensor_or_nested_tensor.apply(
+        cancel_batch, batch_size=[], call_on_nested=True
+    )
+
 
 def _collate_frame(batch: Sequence[Frame]):
 
@@ -25,32 +37,103 @@ def _collate_frame(batch: Sequence[Frame]):
     else:
         n_atoms = coll_batch[alias.n_atoms]
 
-    atom_batch_mask = torch.cat([torch.full((t,), fill_value=i) for i, t in enumerate(n_atoms)]).to(alias.atoms_ns['atom_batch_mask'].dtype)
+    atom_batch_mask = torch.cat(
+        [torch.full((t,), fill_value=i) for i, t in enumerate(n_atoms)]
+    ).to(alias.atoms_ns["atom_batch_mask"].dtype)
 
     atom_offset = torch.zeros(len(n_atoms), dtype=Config.itype)
-    torch.cumsum(torch.flatten(n_atoms)[:-1], dim=0, out=atom_offset[1:]).to(Config.itype)
+    torch.cumsum(torch.flatten(n_atoms)[:-1], dim=0, out=atom_offset[1:]).to(
+        Config.itype
+    )
 
     if alias.pairs in coll_batch:
         n_pairs = torch.tensor(
             [len(frame[alias.pair_i]) for frame in batch], dtype=Config.itype
         )
-        pair_batch_mask = torch.cat([torch.full((t,), fill_value=i) for i, t in enumerate([len(frame[alias.pair_i]) for frame in batch])])
+        pair_batch_mask = torch.cat(
+            [
+                torch.full((t,), fill_value=i)
+                for i, t in enumerate([len(frame[alias.pair_i]) for frame in batch])
+            ]
+        )
         pair_offset = torch.zeros(len(n_pairs), dtype=Config.itype)
-        torch.cumsum(torch.flatten(n_pairs)[:-1], dim=0, out=pair_offset[1:]).to(Config.itype)
+        torch.cumsum(torch.flatten(n_pairs)[:-1], dim=0, out=pair_offset[1:]).to(
+            Config.itype
+        )
+        coll_frame[alias.pair_i] = coll_frame[alias.pair_i] + atom_offset[pair_batch_mask]
+        coll_frame[alias.pair_j] = coll_frame[alias.pair_j] + atom_offset[pair_batch_mask]
+        coll_frame[alias.pair_batch_mask] = pair_batch_mask
+        coll_frame[alias.pair_offset] = pair_offset
 
     coll_frame = coll_batch.apply(cancel_batch, batch_size=[])
     coll_frame[alias.atom_batch_mask] = atom_batch_mask
     coll_frame[alias.atom_offset] = atom_offset
-    coll_frame[alias.pair_i] = coll_frame[alias.pair_i] + atom_offset[pair_batch_mask]
-    coll_frame[alias.pair_j] = coll_frame[alias.pair_j] + atom_offset[pair_batch_mask]
-    coll_frame[alias.pair_batch_mask] = pair_batch_mask
-    coll_frame[alias.pair_offset] = pair_offset
     return coll_frame
 
 
-class DataLoader(DataLoader):
+class MapAndCollate:
+    """A simple transform that takes a batch of indices, maps with dataset, and then applies
+    collate.
+    TODO: make this a standard utility in torchdata.nodes
+    """
+    def __init__(self, dataset, collate_fn):
+        self.dataset = dataset
+        self.collate_fn = collate_fn
 
-    def __init__(self, *args, **kwargs):
-        if "collate_fn" not in kwargs:
-            kwargs["collate_fn"] = _collate_frame
-        super().__init__(*args, **kwargs)
+    def __call__(self, batch_of_indices: list[int]):
+        batch = [self.dataset[i] for i in batch_of_indices]
+        return self.collate_fn(batch)
+    
+
+class DataLoader(tn.Loader):
+
+    def __init__(
+        self,
+        dataset: Dataset,
+        batch_size: int,
+        shuffle: bool = True,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+        drop_last: bool = False,
+        nodes: list[tn.BaseNode] = [],
+        collate_fn=_collate_frame,
+    ):
+        # Assume we're working with a map-style dataset
+        assert hasattr(dataset, "__getitem__") and hasattr(dataset, "__len__")
+        # Start with a sampler, since caller did not provide one
+        sampler = RandomSampler(dataset) if shuffle else SequentialSampler(dataset)
+        # Sampler wrapper converts a Sampler to a BaseNode
+        node = tn.SamplerWrapper(sampler)
+
+        # Now let's batch sampler indices together
+        node = tn.Batcher(node, batch_size=batch_size, drop_last=drop_last)
+
+        # Create a Map Function that accepts a list of indices, applies getitem to it, and
+        # then collates them
+        map_and_collate = MapAndCollate(dataset, collate_fn or default_collate)
+
+        # MapAndCollate is doing most of the heavy lifting, so let's parallelize it. We could
+        # choose process or thread workers. Note that if you're not using Free-Threaded
+        # Python (eg 3.13t) with -Xgil=0, then multi-threading might result in GIL contention,
+        # and slow down training.
+        node = tn.ParallelMapper(
+            node,
+            map_fn=map_and_collate,
+            num_workers=num_workers,
+            method="process",  # Set this to "thread" for multi-threading
+            in_order=True,
+        )
+
+        # Optionally apply pin-memory, and we usually do some pre-fetching
+        if pin_memory:
+            node = tn.PinMemory(node)
+        node = tn.Prefetcher(node, prefetch_factor=num_workers * 2)
+
+        # Note that node is an iterator, and once it's exhausted, you'll need to call .reset()
+        # on it to start a new Epoch.
+        # Insteaad, we wrap the node in a Loader, which is an iterable and handles reset. It
+        # also provides state_dict and load_state_dict methods.
+
+        node = compose(*nodes)(node)
+        
+        super().__init__(node)
--- src/molpot/pipline/dataset.py
+++ src/molpot/pipline/dataset.py
@@ -4,7 +4,7 @@ import random
 import tarfile
 import time
 from pathlib import Path
-from typing import Literal, Sequence
+from typing import Literal, Sequence, Any
 
 import numpy as np
 import requests
@@ -14,31 +14,17 @@ from torch.nn import Module
 import molpot as mpot
 from molpot import Config, NameSpace, alias
 
+from abc import abstractmethod
+
 logger = logging.getLogger("molpot")
 
 
 class Dataset(torch.utils.data.Dataset):
 
-    labels: NameSpace
+    def __init__(self, name: str, save_dir: Path | None = None, device: str = "cpu"):
+        self.labels: NameSpace = NameSpace(name)
+        self.device = device
 
-    def __init__(
-        self,
-        save_dir: Path | None = None,
-        dump_dir: Path | None = None,
-        device: str = "cpu",
-        total: int | None = None,
-    ):
-        """
-        Modified torch.utils.data.Dataset.
-
-        Args:
-            name (str): name of the dataset
-            save_dir (Path | None, optional): determine where and if save original data. It should point to a directory or where will make a directory. Defaults to None.
-            dump_dir (Path | None, optional): determine where and if save processed data, in h5df format. Defaults to None.
-            device (str, optional): Defaults to "cpu".
-            total (int | None, optional): total  . Defaults to None.
-        """
-        super().__init__()
         if save_dir is not None:
             self.save_dir = Path(save_dir)
             if not self.save_dir.exists():  # create save_dir
@@ -46,57 +32,14 @@ class Dataset(torch.utils.data.Dataset):
         else:
             self.save_dir = None
 
-        if dump_dir is not None:
-            self.dump_dir = Path(dump_dir)
-            if not dump_dir.exists():
-                self.dump_dir.mkdir(parents=True, exist_ok=True)
-        else:
-            self.dump_dir = None
-        self.device = device
-        self.labels = NameSpace(self.__class__.__name__)
-        self._preprocess = torch.nn.Sequential()
 
-        self._processes = torch.nn.Sequential()
+class MapStyleDataset(Dataset):
 
-        self._frames = list()
-        self._total = total or len(self._frames)
+    @abstractmethod
+    def __getitem__(self, i: int) -> Any: ...
 
-    @property
-    def frames(self):
-        return self._frames
-
-    def add_process(self, process):
-        self._processes.append(process)
-
-    def apply_processs(self, frame):
-        return self._processes(frame)
-
-    def prepare_data(self):
-        pass
-
-    def __len__(self):
-        return self._total
-
-    def __iter__(self):
-        return iter(map(self.apply_processs, self.frames))
-
-    def __getitem__(self, index):
-        return self.apply_processs(self._frames[index])
-
-    def reset(self):
-        self.state = {}
-
-    def split(self, lengths: Sequence[int | float], generators=None):
-        return torch.utils.data.random_split(
-            self, lengths=lengths, generator=generators
-        )
-
-    def preprocess_data(self):
-        self._frames = [self._preprocess(frame) for frame in self._frames]
-        return self._frames
-    
-    def dump(self, dump_dir: Path):
-        frames = self._frames
+    @abstractmethod
+    def __len__(self): ...
 
 
 class QM9(Dataset):
@@ -181,7 +124,7 @@ class QM9(Dataset):
         return frames
 
 
-class rMD17(Dataset):
+class rMD17(MapStyleDataset):
 
     atomrefs = {
         "energy": [
@@ -196,6 +139,19 @@ class rMD17(Dataset):
             -47069.30768969713,
         ]
     }
+
+    datasets_dict = dict(
+        aspirin="rmd17_aspirin.npz",
+        azobenzene="rmd17_azobenzene.npz",
+        benzene="rmd17_benzene.npz",
+        ethanol="rmd17_ethanol.npz",
+        malonaldehyde="rmd17_malonaldehyde.npz",
+        naphthalene="rmd17_naphthalene.npz",
+        paracetamol="rmd17_paracetamol.npz",
+        salicylic_acid="rmd17_salicylic.npz",
+        toluene="rmd17_toluene.npz",
+        uracil="rmd17_uracil.npz",
+    )
 
     def __init__(
         self,
@@ -212,38 +168,24 @@ class rMD17(Dataset):
             "uracil",
         ],
         save_dir: Path | None = None,
-        dump_dir: Path | None = None,
         device: str = "cpu",
-        total: int = 1000,
+        total: int|None = 1000,
     ):
         super().__init__(
+            name="rmd17",
             save_dir=save_dir,
-            dump_dir=dump_dir,
             device=device,
-            total=total,
         )
-        self.labels.set("energy", "total energy", float, "kcal/mol")
-        self.labels.set("forces", "all forces", float, "kcal/mol/A")
+        self.labels.set(
+            "energy", "total energy", float, "kcal/mol", (None, 1), "labels"
+        )
+        self.labels.set(
+            "forces", "all forces", float, "kcal/mol/A", (None, 3), "labels"
+        )
 
         self.molecule = molecule
 
-        self.total = total
-
-        self.datasets_dict = dict(
-            aspirin="rmd17_aspirin.npz",
-            azobenzene="rmd17_azobenzene.npz",
-            benzene="rmd17_benzene.npz",
-            ethanol="rmd17_ethanol.npz",
-            malonaldehyde="rmd17_malonaldehyde.npz",
-            naphthalene="rmd17_naphthalene.npz",
-            paracetamol="rmd17_paracetamol.npz",
-            salicylic_acid="rmd17_salicylic.npz",
-            toluene="rmd17_toluene.npz",
-            uracil="rmd17_uracil.npz",
-        )
-
-        self.archive_path = self.save_dir / "rmd17.tar.gz"
-        self.molecule_npz_path = self.save_dir / self.molecule / self.datasets_dict[self.molecule]
+        self.total = total if total is not None else torch.inf
 
         molecules = list(self.datasets_dict.keys())
         if molecule not in molecules:
@@ -251,18 +193,20 @@ class rMD17(Dataset):
                 f"Invalid molecule. Choose from {molecules}. Got {molecule}"
             )
 
+        self.archive_path = self.save_dir / "rmd17.tar.gz"
+        self.npz_path = (
+            self.save_dir / self.molecule / self.datasets_dict[self.molecule]
+        )  # save_dir/aspirin/rmd17_aspirin.npz
+
         self.prepare_data()
-        self.preprocess_data()
+
         logger.info(f"Loaded {len(self)} frames")
 
     def __len__(self):
         return self.total
 
     def __getitem__(self, idx):
-        return self.apply_processs(self.frames[idx])
-
-    def __iter__(self):
-        return iter(map(self.apply_processs, self.frames))
+        return self._frames[idx]
 
     @property
     def frames(self):
@@ -271,31 +215,27 @@ class rMD17(Dataset):
     def prepare_data(self):
 
         if self.save_dir is None:
-            frames = self._fetch_data()
+            data = self._fetch_data()
         else:
-            frames = self._download_data(self.save_dir)
-        self._frames = frames
-        if self.dump_dir is not None:
-            self.dump(self.dump_dir)
-        return frames
+            data = self._download_data()
+        self._frames = self.parse_data(data)
+        return self._frames
 
-    def _fetch_data(self):
-        logger.info("Fetching {} data".format(self.molecule))
+    def _fetch_data(self) -> Sequence[mpot.Frame]:
+        logger.info(f"Fetching {self.molecule} data")
         rmd17_url = "https://figshare.com/ndownloader/files/23950376"
         rmd17_bytes = requests.get(rmd17_url, allow_redirects=True).content
         rmd17_fobj = io.BytesIO(rmd17_bytes)
         rmd17_fobj.seek(0)
         rmd17_tar = tarfile.open(fileobj=rmd17_fobj, mode="r:bz2")
-        data = np.load(
-            rmd17_tar.extractfile(self.molecule_npz_path)
-        )
-        return self.parse_data(data)
+        data = np.load(rmd17_tar.extractfile(self.npz_path))
+        return data
 
-    def _download_data(self, save_dir: Path):
+    def _download_data(self) -> Sequence[mpot.Frame]:
 
         url = "https://figshare.com/ndownloader/files/23950376"
         if not self.archive_path.exists():
-            logger.info("Downloading {} data".format(self.molecule))
+            logger.info(f"Downloading {self.molecule} data")
             with requests.get(url, stream=True) as response:
                 response.raise_for_status()
                 with open(self.archive_path, "wb") as file:
@@ -303,22 +243,22 @@ class rMD17(Dataset):
                         file.write(chunk)
             logger.info("Done.")
 
-        if not self.molecule_npz_path.exists():
+        if not self.npz_path.exists():
             logger.info("Extracting data...")
-            # self.molecule_npz_path.parent.mkdir(parents=True, exist_ok=True)
+            # self.npz_path.parent.mkdir(parents=True, exist_ok=True)
             with tarfile.open(self.archive_path) as tar:
                 tar.extract(
-                    path=self.molecule_npz_path.parent,
+                    path=self.npz_path.parent,
                     member=f"rmd17/npz_data/{self.datasets_dict[self.molecule]}",
-                    filter = lambda member, path: member.replace(name=self.datasets_dict[self.molecule])
+                    filter=lambda member, path: member.replace(
+                        name=self.datasets_dict[self.molecule]
+                    ),
                 )
 
-        logger.info("Parsing molecule {:s}".format(self.molecule))
+        logger.info(f"Parsing molecule {self.molecule}")
 
-        data = np.load(
-            self.molecule_npz_path
-        )
-        return self.parse_data(data)
+        data = np.load(self.npz_path)
+        return data
 
     def parse_data(self, data):
         numbers = torch.tensor(data["nuclear_charges"], dtype=Config.itype)
@@ -331,11 +271,11 @@ class rMD17(Dataset):
             frame[alias.R] = torch.tensor(
                 positions, dtype=Config.ftype, requires_grad=True
             )
-            frame["labels"]["energy"] = torch.tensor(
+            frame[self.labels.energy] = torch.tensor(
                 [energies - np.array(self.atomrefs["energy"])[numbers].sum()],
                 dtype=Config.ftype,
             )
-            frame["labels"]["forces"] = torch.tensor(forces, dtype=Config.ftype)
+            frame[self.labels.forces] = torch.tensor(forces, dtype=Config.ftype)
             frame[alias.n_atoms] = torch.tensor([len(numbers)], dtype=Config.itype)
             frame[alias.cell] = torch.zeros(3)
             frame[alias.pbc] = torch.zeros(3, dtype=torch.bool)
