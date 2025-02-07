98eef5e387ba5141785c8725473d0d39ffbb76db
Roy Kid <lijichen365@gmail.com>
2025-02-07 17:41:03 +0000

    update

--- src/molpot/process/nblist.py
+++ src/molpot/process/nblist.py
@@ -16,16 +16,14 @@ class NeighborList(TensorDictModule):
         cutoff: float,
         max_num_pairs: int = -1,
         check_errors: bool = False,
-        index_dtype: torch.dtype = torch.int32,
-        exclude_ii: bool = True,
+        index_dtype: torch.dtype = torch.int32
     ):
-        super().__init__(self, [])
+        super().__init__()
         self.cutoff = cutoff
         self.kernel = partial(
             get_neighbor_pairs, max_num_pairs=max_num_pairs, check_errors=check_errors
         )
         self.index_dtype = index_dtype
-        self.exclude_ii = exclude_ii
 
     def forward(self, inputs):
         """ calculate neighbor list. The pair distance is calculated by xyz[pair_j] - xyz[pair_i]
@@ -45,12 +43,6 @@ class NeighborList(TensorDictModule):
             positions=xyz, box_vectors=box, cutoff=self.cutoff
         )
         pairs = pairs.to(dtype=self.index_dtype)
-        if self.exclude_ii:
-            mask = ~torch.isnan(distances)
-            pairs = pairs[:, mask]
-            deltas = deltas[mask]
-            distances = distances[mask]
-            n_pairs = mask.sum(dim=0)
 
         inputs[alias.pair_i] = pairs[1]
         inputs[alias.pair_j] = pairs[0]
