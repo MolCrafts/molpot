d5e5751363f6cc43c69dbdd33d62ed2b21848cfc
Roy Kid <lijichen365@gmail.com>
2025-03-04 17:10:19 +0000

    update

--- src/molpot/potential/classic/pair/lj.py
+++ src/molpot/potential/classic/pair/lj.py
@@ -66,3 +66,43 @@ class LJ126(LazyModuleMixin, Module):
         power_12 = torch.square(power_6)
         unit_force = 24 * eps * (2 * power_12 - power_6) / torch.square(pair_dist)
         return unit_force.unsqueeze(-1) * pair_diff
+
+
+class Stockmayer(LJ126):
+
+    def __init__(
+        self,
+        sig: torch.Tensor | None = None,
+        eps: torch.Tensor | None = None,
+        mu: torch.Tensor | None = None,
+        xi: torch.Tensor | None = None,
+        calc_forces: bool = False,
+    ):
+        super().__init__(sig, eps, calc_forces)
+        self.mu = mu if isinstance(mu, torch.Tensor) else UninitializedParameter()
+        self.xi = xi if isinstance(xi, torch.Tensor) else UninitializedParameter()
+        self.out_keys.append(("predicts", "stockmayer_energy"))
+
+    def reset_parameters(self) -> None:
+        self.sig = torch.nn.init.uniform_(self.sig, a=0.1, b=1.0)
+        self.eps = torch.nn.init.uniform_(self.eps, a=0.1, b=1.0)
+        self.mu = torch.nn.init.uniform_(self.mu, a=0.1, b=1.0)
+        self.xi = torch.nn.init.uniform_(self.xi, a=0.1, b=1.0)
+
+    def initialize_parameters(self, pair_i, pair_j, pair_diff, atom_types) -> None:  # type: ignore[override]
+        if self.has_uninitialized_params():
+            with torch.no_grad():
+                n_types = atom_types.max().item() + 1
+                self.sig.materialize((n_types, n_types))
+                self.eps.materialize((n_types, n_types))
+                self.mu.materialize((n_types, ))
+                self.xi.materialize((1))
+                self.reset_parameters()
+
+    def energy(self, pair_i, pair_j, pair_dist, atom_types):
+        lj_energy = super().energy(pair_i, pair_j, pair_dist, atom_types)
+        mu1 = self.mu[atom_types[pair_i]]
+        mu2 = self.mu[atom_types[pair_j]]
+        xi = self.xi
+
+        return lj_energy + mu1 * mu2 * xi / torch.square(pair_dist)
